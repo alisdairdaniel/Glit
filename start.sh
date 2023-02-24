1e1839ff9bcb1eda98ba8aca3f67784c6eabdcae
alisdairdaniel <117979215+alisdairdaniel@users.noreply.github.com>
2023-02-23 16:16:07 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # set val
 python3 /app/self-ping.py &
 
-AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+AUUID=6839863e-0fbd-4406-9751-4f5411f36e52
 ParameterSSENCYPT=chacha20-ietf-poly1305
 CADDYIndexPage=https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
 
